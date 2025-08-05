58ac6802b00ee932c413b6752dd3567100f78505
Harshal1551 <harshalpalve1551@gmail.com>
2025-08-05 22:30:15 +0530

    Changes in file name

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,5 +1,5 @@
 
-import logo from "../assets/logo.png"
+import logo from "../assets/Logo.png"
 function Logo({width = '100px'}){
     return(
         <img src={logo} alt="Logo Image" width={width} className="rounded-full animate-mymove"/>
